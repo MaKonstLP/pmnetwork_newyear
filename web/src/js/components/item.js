b88659f8c3bb261d8164b6dec450755f73013dc0
NomarZwill <solovyev@liderpoiska.ru>
2020-03-11 17:10:32 +0400

    main page fix and item page gallery

--- web/src/js/components/item.js
+++ web/src/js/components/item.js
@@ -5,9 +5,40 @@ import 'slick-carousel';
 export default class Item{
 	constructor($item){
 		var self = this;
+		this.bigIMG = document.querySelector(".object_gallery_main");
+		this.listOfIMG = document.querySelector(".object_gallery_other");
 		
-		$('[data-action="show_phone"]').on('click', function(){
-			$('.object_book_hidden').addClass('_active');
-		});			
+		$('[data-action="show_phone"]').on("click", function(){
+			$(".object_book_hidden").addClass("_active");
+		});
+
+		this.initGallery();
+	}
+
+	getIMGFromGallery(e) {
+		let targetIMG = e.target;
+
+		console.log(`${targetIMG.getAttribute("src")}`);
+		document.querySelector(".object_gallery_other").querySelector("._active").classList.toggle("_active");
+
+		if(targetIMG.nodeName == "IMG"){
+			targetIMG.classList.toggle("_active");
+		}
+		let targetIMGSrc = targetIMG.getAttribute("src");
+		document.querySelector(".object_gallery_main").querySelector("img").setAttribute("src", targetIMGSrc);
+	}
+
+	initGallery(){
+		let firstIMG = this.listOfIMG.querySelector("img");
+
+		if( !firstIMG.classList.contains("_active") ){
+			firstIMG.classList.add("_active");
+		}
+
+		let firstIMGSrc = firstIMG.getAttribute("src");
+
+		document.querySelector(".object_gallery_main").querySelector("img").setAttribute("src", firstIMGSrc);
+
+		$(".object_gallery_other").on("click", this.getIMGFromGallery);
 	}
 }
--- web/src/js/components/map.js
+++ web/src/js/components/map.js
@@ -2,29 +2,13 @@
 
 export default class YaMap{
 	constructor(){
+		this.init();
+	}
+
+	init() {
 		ymaps.ready(function(){
 			let map = document.querySelector(".map");
 			let myMap = new ymaps.Map(map, {center: [55.76, 37.64], zoom: 15});
-
-			/*let testData = 
-				{"type": "FeatureCollection", 
-					"features": [
-						{	
-							"type": "Feature",
-							"id": 0,
-							"geometry": {
-								"type": "Point",
-								"coordinates": [55.831903, 37.411961]
-							},
-							"properties": {
-								"balloonContent": "Магазин на углу",
-								"organization": "Магазиин на углу",
-								"address": "Москва, где-то",
-								"img": "img"
-							}
-						}
-					]
-				};*/
 
 			let myBalloonLayout = ymaps.templateLayoutFactory.createClass(
 				`<div class="balloon_layout">
@@ -50,27 +34,6 @@ export default class YaMap{
 
 					this.constructor.superclass.clear.call(this);
 				},
-
-				/*onSublayoutSizeChange: function () {
-					myBalloonLayout.superclass.onSublayoutSizeChange.apply(this, arguments);
-
-					if(!this._isElement(this._$element)) {
-							return;
-					}
-
-					this.applyElementOffset();
-
-					this.events.fire('shapechange');
-				},
-
-				applyElementOffset: function () {
-					this._$element.css({
-							left: -(this._$element[0].offsetWidth / 2),
-							top: -(this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight)
-					});
-					console.log(-(this._$element[0].offsetWidth / 2));
-					console.log(-(this._$element[0].offsetHeight + this._$element.find('.arrow')[0].offsetHeight));
-				},*/
 
 				onCloseClick: function (e) {
 					e.preventDefault();
@@ -156,16 +119,18 @@ export default class YaMap{
 			);
 
 			let objectManager = new ymaps.ObjectManager(
-				{geoObjectBalloonLayout: myBalloonLayout, 
-				geoObjectBalloonContentLayout: myBalloonContentLayout,
-				geoObjectHideIconOnBalloonOpen: false,
-				geoObjectBalloonOffset: [-360, 17],
-				clusterize: true,
-				clusterDisableClickZoom: false,
-				clusterBalloonItemContentLayout: customItemContentLayout
+				{
+					geoObjectBalloonLayout: myBalloonLayout, 
+					geoObjectBalloonContentLayout: myBalloonContentLayout,
+					geoObjectHideIconOnBalloonOpen: false,
+					geoObjectBalloonOffset: [-360, 17],
+					clusterize: true,
+					clusterDisableClickZoom: false,
+					clusterBalloonItemContentLayout: customItemContentLayout,
+					clusterIconColor: "green",
+					geoObjectIconColor: "green"
 				}
 			);
-
 
 			let serverData = null;
 			let serverResponse = fetch("/api/map_all/")
